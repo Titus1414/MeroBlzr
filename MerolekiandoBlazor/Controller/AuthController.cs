581a38e60976d9b2125f3e2016c5937cbde95774
titus Zaman <titus.ati@hotmail.com>
2023-05-09 11:30:51 +0500

    Auth Work

---
+++ MerolekiandoBlazor/Controller/AuthController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.Facebook;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.EntityFrameworkCore;
+using HtmlAgilityPack;
+using Azure;
+
+namespace MerolekiandoBlazor.Controller
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("WebLogin")]
+        public async Task<ActionResult> WebLogin(/*string flg*/)
+        {
+
+            
+            var properties = new AuthenticationProperties {
+
+                RedirectUri = "/"
+            };
+
+
+
+            return Challenge(properties, FacebookDefaults.AuthenticationScheme);
+
+            //if (flg == "goog")
+            //{
+            //    return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+
+            //    //await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme, new AuthenticationProperties()
+            //    //{
+            //    //    RedirectUri = Url.Action("GoogleResponse")
+            //    //});
+            //    //return RedirectToAction("GoogleResponse");
+            //}
+            //else
+            //{
+            //    return Challenge(properties, FacebookDefaults.AuthenticationScheme);
+
+            //    //await HttpContext.ChallengeAsync(FacebookDefaults.AuthenticationScheme, new AuthenticationProperties()
+            //    //{
+            //    //    RedirectUri = Url.Action("FacebookResponse")
+            //    //});
+            //    //return RedirectToAction("FacebookResponse");
+            //}
+        }
+        public async Task<IActionResult> GoogleResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var claim = result.Principal.Identities
+                .FirstOrDefault().Claims
+                .Select(claim => new
+                {
+                    claim.Issuer,
+                    claim.OriginalIssuer,
+                    claim.Type,
+                    claim.Value
+                }
+                );
+            //User usr = new();
+            //usr.LoginType = "Google";
+            //foreach (var item in claim)
+            //{
+            //    if (item.Type.Contains("/nameidentifier"))
+            //    {
+            //        usr.UniqueId = item.Value;
+            //    }
+            //    else if (item.Type.Contains("/name"))
+            //    {
+            //        usr.Name = item.Value;
+            //    }
+            //    else if (item.Type.Contains("/emailaddress"))
+            //    {
+            //        usr.Email = item.Value;
+            //    }
+            //}
+            return RedirectToAction("WebLoginT"/*, usr*/);
+        }
+        public async Task<IActionResult> FacebookResponse()
+        {
+            var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var claim = result.Principal.Identities
+                .FirstOrDefault().Claims.Select(claim => new
+                {
+                    claim.Issuer,
+                    claim.OriginalIssuer,
+                    claim.Type,
+                    claim.Value
+                });
+            //User usr = new();
+            //usr.LoginType = "Facebook";
+            //foreach (var item in claim)
+            //{
+            //    if (item.Type.Contains("/nameidentifier"))
+            //    {
+            //        usr.UniqueId = item.Value;
+            //    }
+            //    else if (item.Type.Contains("/name"))
+            //    {
+            //        usr.Name = item.Value;
+            //    }
+
+            //}
+            return RedirectToAction("WebLoginT"/*, usr*/);
+        }
+        public async Task<IActionResult> WebLogout()
+        {
+            HttpContext.Session.Remove("WebUserId");
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("WebIndex");
+        }
+        
+    }
+}
--- MerolekiandoBlazor/Program.cs
+++ MerolekiandoBlazor/Program.cs
@@ -1,15 +1,84 @@
 using Blazored.Modal;
 using MerolekiandoBlazor.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile("appsettings.json", optional: true);
+});
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddBlazoredModal();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddAuthentication(
+    options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    //options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+    //options.DefaultForbidScheme = FacebookDefaults.AuthenticationScheme;
+}
+)
+    .AddCookie(options => options.SlidingExpiration = true)
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateActor = false,
+            ValidateLifetime = true,
+            NameClaimType = ClaimTypes.NameIdentifier
+
+        };
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                var accessToken = context.Request.Query["access_token"];
+                var path = context.HttpContext.Request.Path;
+                if (!string.IsNullOrEmpty(accessToken) && (path.StartsWithSegments("/signalServer"))) // for me my hub endpoint is ConnectionHub
+                {
+                    context.Token = accessToken;
+                }
+                return Task.CompletedTask;
+            }
+        };
+        //GoogleDefaults.AuthenticationScheme,
+    })
+.AddGoogle(options =>
+ {
+     options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+     options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+     options.ClaimActions.MapJsonKey("urn:google:picture", "picture", "url");
+     //FacebookDefaults.AuthenticationScheme, 
+ }).AddFacebook(options =>
+ {
+     options.AppId = builder.Configuration["Authentication:Facebook:AppId"];
+     options.AppSecret = builder.Configuration["Authentication:Facebook:AppSecret"];
+ });
+
 
 var app = builder.Build();
 
@@ -26,8 +95,13 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.UseEndpoints(endpoits =>
+{
+    endpoits.MapControllers();
+});
 
 app.Run();
